d9ba9c485f0b8a4f946666373824b5a32436cf0a
Troy Whorten <troy.whorten@wyo.gov>
2014-05-27 15:53:43 -0600

    Added basic Google Maps functionality

---
+++ NerdDinner/Controllers/DinnerViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NerdDinner.Model;
+
+namespace NerdDinner.Controllers
+{
+    public class DinnerViewModel
+    {
+        public const int PageSize = 25;
+
+        public Dinner SelectedDinner { get; set; }
+        public int RSVPs { get; set; }
+        string _currentUser;
+        public string CurrentUser
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_currentUser))
+                {
+                    if (HttpContext.Current != null)
+                        _currentUser = HttpContext.Current.User.Identity.Name;
+                }
+                return _currentUser;
+            }
+
+            set
+            {
+                _currentUser = value;
+            }
+        }
+
+        public DateTime DatePrompt
+        {
+            get
+            {
+                return DateTime.Now.AddDays(7);
+            }
+        }
+
+        public string TimePrompt
+        {
+            get
+            {
+                return "6:00pm";
+            }
+        }
+
+        public bool IsCurrentUserOwner
+        {
+            get
+            {
+                return SelectedDinner.HostedBy.Equals(CurrentUser, StringComparison.InvariantCultureIgnoreCase);
+            }
+        }
+    }
+}
---
+++ NerdDinner/Scripts/nerd.js
@@ -0,0 +1,41 @@
+﻿// by Troy Whorten, Feb 28 2014 (though to be fair I didn't write much of it...)
+
+function getLoc() {
+    if (geoPosition.init()) {
+        geoPosition.getCurrentPosition(geoSuccess, geoError);
+    }
+}
+
+var map;
+
+function geoSuccess(p) {
+    //document.getElementById("latitude").innerHTML = p.coords.latitude;
+    //document.getElementById("longitude").innerHTML = p.coords.longitude;
+    //document.getElementById("accuracy").innerHTML = p.coords.accuracy;
+
+    // from Google Maps API 
+    // https://developers.google.com/maps/documentation/javascript/examples/map-simple
+    // I just swaped out lat and long for Syndney, Australia for the geolocation info above
+    var mapOptions = {
+        zoom: 15,
+        center: new google.maps.LatLng(p.coords.latitude, p.coords.longitude)
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    // this I copied from a different Google Maps API example
+    // https://developers.google.com/maps/documentation/javascript/examples/map-geolocation
+    var pos = new google.maps.LatLng(p.coords.latitude,
+                                     p.coords.longitude);
+
+    var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+    });
+
+    //end Google API Code
+}
+
+function geoError() {
+    document.getElementById("errormsg").innerHTML = "Could not find you!";
+}
